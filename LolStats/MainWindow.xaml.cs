618a605bc437b3cf856933f59e1e9387b2e2da40
ordontomasz <ordon.tomasz@gmail.com>
2022-10-16 13:02:21 +0200

    Config & config manager api key

--- LolStats/MainWindow.xaml.cs
+++ LolStats/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
@@ -36,22 +37,22 @@ namespace LolStats
         {
             string summonerName = SummonerName.Text;
             string region = RegionComboBox.Text;
-            //Trace.WriteLine(summonerName);
-            //Trace.WriteLine(region);
+
+            string apiKey = ConfigurationManager.AppSettings["RiotApiKey"].ToString();
 
             if (!String.IsNullOrEmpty(summonerName))
             {
+                if (region == "EUNE")
+                    region = "eun1.api.riotgames.com";
+                if (region == "EUW")
+                    region = "euw1.api.riotgames.com";
+                if (region == "NA")
+                    region = "americas.api.riotgames.com";
+
                 using (HttpClient client = new HttpClient())
                 {
-                    if (region == "EUNE")
-                        region = "eun1.api.riotgames.com";
-                    if (region == "EUW")
-                        region = "euw1.api.riotgames.com";
-                    if (region == "NA")
-                        region = "americas.api.riotgames.com";
+                    var response = await client.GetAsync("https://" + region + "/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + apiKey);
 
-                    var response = await client.GetAsync("https://" + region + "/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=RGAPI-5947969f-14ad-4cae-83c1-b378e37d0a50");
-                    //response.EnsureSuccessStatusCode();
                     if (response.IsSuccessStatusCode)
                     {
                         string responseText = await response.Content.ReadAsStringAsync();
@@ -60,12 +61,9 @@ namespace LolStats
                         string summonerLevel = data.summonerLevel;
                         Trace.WriteLine(temp);
                         Trace.WriteLine("Poziom gracza: " + summonerLevel);   
-                        //Trace.WriteLine("Poziom: " + data.summonerLevel);
                     }
                     else
-                    {
                         Trace.WriteLine("Error API");
-                    }
                 }     
             }
 
